b58d39cc6588604a62fca8d548e095ac333a4848
Jouni Huttunen <jouni.huttunen@solita.fi>
2013-03-20 13:06:45 +0200

    Localize episerver cms menu title

--- Solita.LanguageEditor.UI/Controllers/LanguageEditorController.cs
+++ Solita.LanguageEditor.UI/Controllers/LanguageEditorController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Web.Mvc;
 using EPiServer.Shell.Navigation;
 using Solita.LanguageEditor.UI.Common;
+using Solita.LanguageEditor.UI.Lang;
 using Solita.LanguageEditor.UI.Models;
+using EPiServer.Shell.Web.Mvc.Html;
 
 namespace Solita.LanguageEditor.UI.Controllers
 {
@@ -15,7 +17,7 @@ namespace Solita.LanguageEditor.UI.Controllers
             _persister = new LocalizationPersister();
         }
 
-        [MenuItem("/global/cms/localizations", Text = "Language editor")]
+        [MenuItem("/global/cms/localizations", TextResourceKey = "CmsMenuTitle", ResourceType = typeof(CmsMenuTitleProvider))]
         public ActionResult Index()
         {
             // For some reason EPiServer's add-on system doesn't check for .cshtml files, so the view path needs to be set manually
