ea5b2bd0b7b83f9acc9edb0980cba3b21f78d106
Jukka Hyvärinen <jukka.hyvarinen@solita.fi>
2015-09-22 15:17:51 +0300

    adding tests for current code

---
+++ Solita.LocalizationEditor.Tests/LocalizationHelpersTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solita.LocalizationEditor.Definitions;
+
+namespace Solita.LocalizationEditor.Tests 
+{
+
+    /// <summary>
+    /// Unit tests for <see cref="LocalizationHelpers"/>
+    /// </summary>
+    [TestClass]
+    public class LocalizationHelpersTests 
+    {
+
+        [TestMethod]
+        public void GetLocalizationDefinitions()
+        {
+            
+            var result = LocalizationHelpers.GetLocalizationDefinitions();
+
+            Assert.IsNotNull(result, "result");
+            Assert.AreEqual(2, result.Count, "Number of localization definitions");
+
+            var first = result[0];
+            Assert.AreEqual("First", first.Description, "first.Description");
+            Assert.AreEqual("First", first.DefaultValue, "first.DefaultValue");
+            Assert.AreEqual("Test", first.Category, "first.Category");
+            Assert.AreEqual("/test/first", first.Key, "first.Key");
+
+            var second = result[1];
+            Assert.AreEqual("Second", second.Description, "second.Description");
+
+        }
+
+    }
+
+}
---
+++ Solita.LocalizationEditor.Tests/TestData/TestLocalizationCategory.cs
@@ -0,0 +1,21 @@
+﻿using Solita.LocalizationEditor.Definitions;
+
+namespace Solita.LocalizationEditor.Tests.TestData 
+{
+
+    [LocalizationCategory("Test", 1)]
+    public class TestLocalizationCategory 
+    {
+
+        [Localization(Description = "First", DefaultValue = "First")]
+        public const string First = "/test/first";
+
+        [Localization(Description = "Second", DefaultValue = "Second")]
+        public const string Second = "/test/second";
+
+        // Property without LocalizationAttribute is ignored
+        public const string Third = "/test/third";
+
+    }
+
+}
