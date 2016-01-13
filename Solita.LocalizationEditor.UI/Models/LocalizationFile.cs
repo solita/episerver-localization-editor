97e74440105be725c596541323031ed8c142232d
Jukka Hyvärinen <jukka.hyvarinen@solita.fi>
2016-01-13 13:21:12 +0200

    obsoleted LocalizationFile media type.

--- Solita.LocalizationEditor.UI/Models/LocalizationFile.cs
+++ Solita.LocalizationEditor.UI/Models/LocalizationFile.cs
@@ -1,5 +1,7 @@
-﻿using EPiServer.Core;
+﻿using System;
+using EPiServer.Core;
 using EPiServer.DataAnnotations;
+using EPiServer.Framework.DataAnnotations;
 
 namespace Solita.LocalizationEditor.UI.Models
 {
@@ -7,6 +9,8 @@ namespace Solita.LocalizationEditor.UI.Models
         GUID = "0E99DF44-8473-45AB-989B-6EF54ADF1D10",
         DisplayName = "LocalizationResult file"
         )]
+    [Obsolete("To be removed")]
+    [MediaDescriptor(ExtensionString = "localizationFile")]
     public class LocalizationFile : MediaData
     {
         
